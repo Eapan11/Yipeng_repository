3317622aaefca93dce9fa620cae9a36f242af622
Eapan11 <983472363@qq.com>
2024-11-09 22:19:10 +0800

    hyp/1108
    Date:      Sat Nov 9 22:19:10 2024 +0800
    
    On branch master
    Initial commit
    Changes to be committed:
    	new file:   CMakelists.txt
    	new file:   README.md
    	new file:   main.cpp

---
+++ README.md
@@ -0,0 +1,13 @@
+# README
+### This is a testing repository/hyp
+- Cpp
+- Autonomous Driving
+
+### Start the project
+- change the default compiler of cmake on Windows platform! ~~cmake ..~~
+```
+mkdir cmake && cd cmake
+cmake -G "MinGW Makefiles" ..
+make
+./Myproject.exe
+```
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+class CutinReasoning {
+public:
+    CutinReasoning() = default;
+    CutinReasoning(const double &speed, const double &currRadius) : currSpeed(speed), currRadius(currRadius) {};
+    void InitData(const double &speed, const double &currRadius);
+
+    const float GetAcc()
+    {
+        return currSpeed * currSpeed / currRadius;
+    }
+
+    ~CutinReasoning() {}
+private:
+    double currSpeed {0.0};
+    double currRadius {1.0};
+};
+
+void CutinReasoning::InitData(const double &speed, const double &currRadius)
+{
+    currSpeed = speed;
+    this->currRadius = currRadius;
+}
+
+int main()
+{
+    CutinReasoning car;
+    car.InitData(2.0, 3.0);
+    std::cout << "-----Testing-----" << std::endl;
+    std::cout << car.GetAcc() << std::endl;
+} 
